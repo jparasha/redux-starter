8daf27f4e1678d246a0a86011fa4c48d32ee9be6
jparasha <jayant.parashar@capgemini.com>
2018-04-03 16:47:40 +0530

    added action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,7 @@
 export function selectBook(book){
-    console.log('Book Selected-> '+ book.title);
+    //console.log('Book Selected-> '+ book.title);
+    return{
+        type :'BOOK_SELECTED',
+        payload:book
+    }
 }
